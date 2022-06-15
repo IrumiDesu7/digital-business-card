760a2ef08f5560b75044fad6a5e480dac387a2d9
IrumiDesu7 <ikalam89@gmail.com>
2022-06-15 21:39:56 +0800

    edit about section

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,12 @@ export default function About() {
     <div className='about'>
       <h3 className='about-title'>About</h3>
       <p className='about-paragraph'>
-        I am a frontend developer with a particular interest in making things
-        simple and automating daily tasks. I try to keep up with security and
-        best practices, and am always looking for the new things to learn.
+        After deciding that I want to be a full stack developer and try learning
+        it, I realized that web development is a passion to me and I really love
+        doing it. So I set myself to learn everyday to be better and I believe
+        that progress is always a good thing even if it’s just a little
+        progress. So here I am pursuing my dream to become a full-stack
+        developer.
       </p>
     </div>
   );
