b32cd48c7a00921fef0a4986bab65b7daf2c926b
IrumiDesu7 <ikalam89@gmail.com>
2022-06-15 12:21:22 +0800

    add About section

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,12 @@
 export default function About() {
-  return <h1>This is About</h1>;
+  return (
+    <div className='about'>
+      <h3 className='about-title'>About</h3>
+      <p className='about-paragraph'>
+        I am a frontend developer with a particular interest in making things
+        simple and automating daily tasks. I try to keep up with security and
+        best practices, and am always looking for the new things to learn.
+      </p>
+    </div>
+  );
 }
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,15 +1,23 @@
 import myPic from '../img/image.png';
+import mail from '../img/mailIcon.png';
+import linkedIn from '../img/linkedInIcon.png';
 
 export default function Info() {
   return (
     <div className='info'>
-      <img src={myPic} alt='person' />
+      <img className='profilePic' src={myPic} alt='person' />
       <h2 className='name'>Irumi</h2>
       <h5 className='position'>Frontend Developer</h5>
       <h6 className='website'>irumi.website</h6>
       <div className='buttons'>
-        <button className='email'>Email</button>
-        <button className='linkedin'>LinkedIn</button>
+        <button className='email'>
+          <img className='emailIcon' src={mail} alt='' />
+          Email
+        </button>
+        <button className='linkedin'>
+          <img className='linkedInIcon' src={linkedIn} alt='' />
+          LinkedIn
+        </button>
       </div>
     </div>
   );
